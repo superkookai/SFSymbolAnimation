e6f8302baf4c2c2a453c19d9dbfa75aab565384d
Weerawut Chaiyasomboon <weerawut@live.com>
2024-02-21 09:07:35 +0700

    Initial Commit

---
+++ SFSymbolAnimation/SFSymbolAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolAnimationApp.swift
+//  SFSymbolAnimation
+//
+//  Created by Weerawut Chaiyasomboon on 21/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
